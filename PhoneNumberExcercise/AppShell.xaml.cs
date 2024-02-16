7a9626115b664a513af7f4b81d64c1ecf5f01ec9
jp3pe <rlaworhkd430430@gmail.com>
2024-02-16 19:01:19 +0900

    초기 커밋.

---
+++ PhoneNumberExcercise/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneNumberExcercise
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PhoneNumberExcercise/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhoneNumberExcercise
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
